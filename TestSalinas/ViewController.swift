36a695d56b9439f4609f64ba6bf6327266a62be2
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2019-06-05 12:22:57 -0500

    Initial Commit

---
+++ TestSalinas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSalinas
+//
+//  Created by MacbookPro on 6/5/19.
+//  Copyright © 2019 nut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
