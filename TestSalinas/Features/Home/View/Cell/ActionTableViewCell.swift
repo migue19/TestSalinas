123b131a0ea71c30e78c69cc2ac8f38098e28ee2
Miguel Mexicano Herrera <miguel.mexicano@kavak.com>
2021-11-15 19:00:51 -0600

    add Take selfie cell

---
+++ TestSalinas/Features/Home/View/Cell/ActionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActionTableViewCell.swift
+//  TestSalinas
+//
+//  Created by Miguel Mexicano Herrera on 15/11/21.
+//  Copyright © 2021 nut. All rights reserved.
+//
+
+import UIKit
+
+class ActionTableViewCell: UITableViewCell {
+    static let identifier = String(describing: ActionTableViewCell.self)
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TestSalinas/Features/Home/View/HomeVC.swift
+++ TestSalinas/Features/Home/View/HomeVC.swift
@@ -17,6 +17,7 @@ class HomeVC: BaseViewController {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UINib(nibName: InputTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: InputTableViewCell.identifier)
+        tableView.register(UINib(nibName: ActionTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: ActionTableViewCell.identifier)
     }
 }
 /// Protocolo para recibir datos de presenter.
@@ -35,17 +36,51 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource {
             }
             return cell
         case 1:
-            return UITableViewCell()
+            guard let cell = self.tableView.dequeueReusableCell(withIdentifier: ActionTableViewCell.identifier) as? ActionTableViewCell else {
+                return UITableViewCell()
+            }
+            return cell
         default:
             return UITableViewCell()
         }
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
+        switch indexPath.row {
+        case 0:
             return 60
+        case 1:
+            return 70
+        default:
+            return 45
         }
-        return 45
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 1:
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.allowsEditing = true
+            picker.delegate = self
+            present(picker, animated: true)
+        default:
+            break
+        }
+    }
+}
+extension HomeVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
 
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        dismiss(animated: true)
+    }
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
